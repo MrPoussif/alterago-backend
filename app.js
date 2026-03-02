4d14199200008341cbd42d4672b4a842cf6f2e5d
Clement <clement45dps@gmail.com>
2026-03-02 13:00:57 +0100

    création route recettes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require("morgan");
 require("./models/connection");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var recettesRouter = require("./routes/recettes");
 
 var app = express();
 const cors = require("cors");
@@ -20,5 +21,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/recettes", recettesRouter);
 
 module.exports = app;
