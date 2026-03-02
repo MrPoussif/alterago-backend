84719c00fb52d9263c658d5d92e4d1031e00ec60
Faysal <soufay75@gmail.com>
2026-03-02 17:36:55 +0100

    ajout route

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ require("./models/connection");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var recettesRouter = require("./routes/recettes");
-
+var challengesRouter = require("./routes/challenges");
 var app = express();
 const cors = require("cors");
 app.use(cors());
@@ -23,5 +23,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/recettes", recettesRouter);
+app.use("/challenges", challengesRouter);
 
 module.exports = app;
